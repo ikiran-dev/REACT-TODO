5897783fc65264a2bf2d7a84c817e4c40504b2b1
Kiran Kumar u <59160552+ikiran-dev@users.noreply.github.com>
2023-09-13 00:33:33 +0530

    Update axios.js

--- todo-frontend/src/axios.js
+++ todo-frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://react-todo-api-git-master-ikiran-dev.vercel.app/",
 });
 
 export default instance;
